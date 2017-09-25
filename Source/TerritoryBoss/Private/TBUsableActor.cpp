6bfd3ca09da077306ef681380569cbc6d61b30b5
endiliey <endiliey@gmail.com>
2017-09-25 12:22:38 +0800

    Outline Post Processing

--- Source/TerritoryBoss/Private/TBUsableActor.cpp
+++ Source/TerritoryBoss/Private/TBUsableActor.cpp
@@ -21,19 +21,12 @@ void ATBUsableActor::OnBeginFocus()
 {
 	// Used by custom PostProcess to render outlines
 	MeshComp->SetRenderCustomDepth(true);
-	MeshComp->CustomDepthStencilValue = 255;
-
-	UE_LOG(LogTemp, Warning, TEXT("render"));
 }
 
 void ATBUsableActor::OnEndFocus()
 {
 	// Used by custom PostProcess to render outlines
 	MeshComp->SetRenderCustomDepth(false);
-
-
-	UE_LOG(LogTemp, Warning, TEXT("unrender"));
-
 }
 
 
